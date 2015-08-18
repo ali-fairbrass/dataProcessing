061ae39c103b46f83796606bd8c860ac29692158
ali-fairbrass <alison.fairbrass@gmail.com>
2015-08-18 17:17:43 +0100

    Created function to crop SM2 files in single folder

---
+++ cropSM2Files.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+from scipy.io import wavfile
+import os
+import glob
+
+def cropSM2Files(inputdirectory, outputdirectory):
+
+    crop_duration = 60.0  # seconds
+
+    if not os.path.exists(outputdirectory):
+        os.makedirs(outputdirectory)
+
+    audio_files = glob.glob(inputdirectory + "\\" + '*.wav')
+
+    for ii, file_name_full in enumerate(audio_files):
+        
+        for i in xrange(30):
+            
+            start_time = str(i*6) + str(.0) # seconds    
+
+            file_name = os.path.basename(file_name_full)
+            sampling_rate, x_full = wavfile.read(file_name_full)
+            file_duration = x_full.shape[0] / float(sampling_rate)
+
+            print '\n', os.path.basename(file_name), start_time
+
+    #         crop
+            x_crop = x_full[sampling_rate*float(start_time):sampling_rate*(float(start_time)+crop_duration)]
+
+            # save
+            op_file_name = outputdirectory + "\\" + file_name[:-4] + str(i) + '.wav'
+            wavfile.write(op_file_name, sampling_rate, x_crop)
+
+
+cropSM2Files("C:\\Users\\ucfaalf\\Documents\\Projects\\AcousticAnalysis\\test29mins", "C:\\Users\\ucfaalf\\Documents\\Projects\\AcousticAnalysis\\test1mins")
+                
