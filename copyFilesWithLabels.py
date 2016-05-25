fc3c2da8ef24c95362ebdd912d7211cf96eb7ce5
ali-fairbrass <alison.fairbrass@gmail.com>
2016-05-25 21:15:41 +0100

    Removed superfluous code

--- copyFilesWithLabels.py
+++ copyFilesWithLabels.py
@@ -1,31 +1,23 @@
-# import os
-# import glob
-# import shutil
-# import csv
+import os
+import glob
+import shutil
+import csv
 
-# # wavFileList = []
-# # labelFileList = []
+wavFileList = []
+labelFileList = []
 
-# # DIR = "C:\\Users\\ucfaalf\\Documents\\Dropbox\\EngD\\Data\\2013Random\\"
-# # wavFiles = os.listdir(DIR + "extra30From6_24kHz")
-# # labelFiles = os.listdir(DIR + "extra30From6_labels")
+DIR = "C:\\Users\\ucfaalf\\Documents\\Dropbox\\EngD\\Projects\\Chapter4\\infrasonicDiversityTest\\"
+labelFiles = os.listdir(DIR + "extra25BioticLabels")
 
-# # for wavFile in wavFiles:
-# # 	wavFileList.append(wavFile[:-4])
-# # print wavFileList
+for label in labelFiles:
+	labelFileList.append(label[:-14])
 
-# # for label in labelFiles:
-# # 	labelFileList.append(label[:-14])
-# # print labelFileList
 
-# # birdWav = [x for x in wavFileList if x in labelFileList]
-# # print birdWav
+for i in labelFileList:
+	print "Copying " + str(i)
+	shutil.copy(DIR + "extra25Recordings\\" + str(i) + ".wav", DIR + "second25BioticRecordings")
 
-# # for i in birdWav:
-# # 	print "Copying " + str(i)
-# # 	shutil.copy(DIR + "extra30From6_24kHz\\" + str(i) + ".wav", DIR + "extra30from6_birdFiles")
-
-# # #Write filenames to csv file
-# # with open(DIR + '\\birdFiles.csv', 'wb') as f:
-# #     writer = csv.writer(f)
-# #     writer.writerows([sorted(birdWav)])
+# #Write filenames to csv file
+# with open(DIR + '\\birdFiles.csv', 'wb') as f:
+#     writer = csv.writer(f)
+#     writer.writerows([sorted(birdWav)])
