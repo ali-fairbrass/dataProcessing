db3faf02e67c9beaa986cf386275f930cd67fa21
ali-fairbrass <alison.fairbrass@gmail.com>
2017-01-20 17:41:23 +0000

    Script to generate csv of seconds of a group of sounds per file

---
+++ calculateTimeForSoundGroup.py
@@ -0,0 +1,109 @@
+import csv
+import numpy as np
+import os
+import pandas as pd
+from pandas import read_csv
+import itertools
+
+def getCSVFileList(csvfileDirectory):
+	csvfileList = os.listdir(csvfileDirectory) 
+	return csvfileList
+
+def getWavWOLabels(wavfileDirectory, csvfileList):
+
+	wavfileList = os.listdir(wavfileDirectory) 
+
+	csvfileListBase = []
+	for csvFile in csvfileList:
+	    csvBaseName = csvFile[:-14]
+	    csvfileListBase.append(csvBaseName)
+	    
+	wavFileListBase = []
+	for wavFile in wavfileList:
+	    wavBaseName = wavFile[:-4]
+	    wavFileListBase.append(wavBaseName)
+
+	wavWOcsv = []
+	for wav in wavFileListBase:
+	    if wav not in csvfileListBase:
+	        wavWOcsv.append(wav)
+
+	return wavWOcsv, wavfileList
+
+def getSecondsWithSound(label_subset):
+    
+    allSecondsList = []
+
+    for row in xrange(0, len(label_subset.index), 1):
+        startTime = label_subset['LabelStartTime_Seconds'].iloc[row]
+        endTime = label_subset['LabelEndTime_Seconds'].iloc[row]
+        secondsList = np.arange(startTime,endTime,0.01).tolist()
+        allSecondsList.append(secondsList)
+    return allSecondsList
+
+def getTimeForSoundGroup(df, soundGroupList):
+
+    allGroupSeconds = []
+
+    for sound in soundGroupList:
+    	# print sound
+        label_subset = df[df['Label'] == sound]
+        if label_subset.shape[0] != 0:
+            soundSecondsList = getSecondsWithSound(label_subset)
+            # print soundSecondsList
+            allGroupSeconds.append(soundSecondsList)
+        else:
+            pass
+
+    merged = list(itertools.chain(*allGroupSeconds))
+    merged = list(itertools.chain(*merged))
+    myFormattedList = [ '%.2f' % elem for elem in merged ]
+    lst = list(set(myFormattedList))
+    totalTimeForSoundGroup = ("%.2f" % (len(lst)*0.01))
+    # print totalTimeForSoundGroup
+
+    return totalTimeForSoundGroup
+
+def createResults(wavfileList, wavWOcsv, csvfileDirectory):
+
+	results = []
+
+	for wavFile in wavfileList:
+	    row = []
+	    if wavFile[:-4] in wavWOcsv:
+	        row.append([wavFile] + [str(0.00)])
+	        row = list(itertools.chain(*row))
+	        # print row
+	        results.append(row)
+	        # print results
+	    else:
+	        filePath = csvfileDirectory + '/' + wavFile[:-4] + '-sceneRect.csv' # Changed from "_below12kHz.csv" for SM2BAT data
+	        df = read_csv(filePath)
+	        row.append(wavFile)
+	        totalTimeForSoundGroup = getTimeForSoundGroup(df, soundGroupList)
+	        row.append(totalTimeForSoundGroup)
+			# print row
+    		results.append(row)
+	return results
+
+def writeResultsToCSV(resultsFileDirectory, resultsFileName, soundGroupName, results):
+	outputFile = open(resultsFileDirectory + "\\" + resultsFileName + ".csv", "w")
+	writer = csv.writer(outputFile, delimiter=',')
+	columnHeader = ["Site"] + soundGroupName
+	writer.writerow(columnHeader)
+	writer.writerows(results)
+	outputFile.close()
+
+soundGroupList = ["Animal", "Wing Beats", "Insect", "Bird"]
+soundGroupName = ["Biotic"]
+
+csvFolder = "C:\\Users\\ucfaalf\\Dropbox\\EngD\\Projects\\Chapter3 Classifier Evaluation\\goldenTestSet\\40LabelFiles\\Golden"
+wavFolder = 'C:\\Users\\ucfaalf\\Dropbox\\EngD\\Projects\\Chapter3 Classifier Evaluation\\goldenTestSet\\40WavFiles'
+
+resultsFolder = "C:\\Users\\ucfaalf\\Dropbox\\EngD\\Projects\\Chapter3 Classifier Evaluation\\goldenTestSet\\labelTimes"
+resultsName = "golden_BioticLabelTimes"
+
+csvfileList = getCSVFileList(csvFolder)
+wavWOcsv, wavfileList = getWavWOLabels(wavFolder, csvfileList)
+results = createResults(wavfileList, wavWOcsv, csvFolder)
+writeResultsToCSV(resultsFolder, resultsName, soundGroupName, results)
